89faadd569b4d85d55aa6073fb6a4d3aa3b83277
bikrom <bikromroy0711@gmail.com>
2022-11-29 22:54:00 +0600

    add line

---
+++ Fibonacci.java
@@ -0,0 +1,8 @@
+public class fibonacci {
+    public static void main(String[] args) {
+        int n=10;
+        for(int i=0;i<n;i++){
+            System.out.println("The number is : " +i ); //invoking the recursive method!!!
+        }
+    }
+}
