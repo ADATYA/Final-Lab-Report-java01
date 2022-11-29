54206a663188227fb140d43935661fd307fc0946
bikrom <bikromroy0711@gmail.com>
2022-11-30 01:43:25 +0600

    add line

---
+++ Interface/abstract.java
@@ -0,0 +1,15 @@
+interface Test {
+    void move();
+}
+class arithmatic{
+    public void move(){
+        System.out.println("\n A mathamatic term is more importent in CSE\n" +
+                            "Now I am realized that it is to much herd to crack it daily");
+    }
+}
+public class test{
+    public static void main(String[] args) {
+        arithmatic a=new arithmatic();
+        a.move();
+    }
+}
