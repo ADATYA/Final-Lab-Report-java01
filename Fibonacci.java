0fb614bbf0cf4a7fb4203984e2ab2760cb70fc3e
bikrom <bikromroy0711@gmail.com>
2022-11-30 01:55:06 +0600

    add line

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,8 +1,22 @@
-public class fibonacci {
-    public static void main(String[] args) {
-        int n=10;
-        for(int i=0;i<n;i++){
-            System.out.println("The number is : " +i ); //invoking the recursive method!!!
-        }
+import java.util.*;
+public class Math {
+    public  static int Math(int a){
+        if(a==1)
+            return 0;
+            else if(a==2)
+                return 1;
+            else
+                return  Math(a-1)+Math(a-2);
+
+
     }
+
+    public static void main(String[] args) {
+        Scanner input =new Scanner (System.in);
+        System.out.print("Type a Math term is:");
+        int a= input.nextInt();
+        int b=Math(a);
+        System.out.println("The Math is :: "+b);
+    }
+
 }
