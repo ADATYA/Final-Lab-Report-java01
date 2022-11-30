45951d71cf650d2ca7edbd09d648120151d549d3
bikrom <bikromroy0711@gmail.com>
2022-11-30 23:14:40 +0600

    new line

---
+++ Exception1.java
@@ -0,0 +1,17 @@
+import java.sql.SQLOutput;
+
+public class Throws {
+    public static void main(String[] args) {
+        int S=8,B=0;
+        try{
+            int A=S/B;
+            if (A==0)
+                throw new Exception("This is imposiable");
+            System.out.println(A);
+
+        }catch (Exception e){
+            System.out.println("Print ERROR !!!  " +e);
+        }
+
+    }
+}
